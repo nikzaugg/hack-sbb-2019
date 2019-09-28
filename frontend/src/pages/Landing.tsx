98bb58349092ea3a2797bba4f81d796ee934c414
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-09-28 21:29:04 +0200

    Semantic loader

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -2,42 +2,54 @@ import React from 'react'
 import { SearchForm } from '../components/SearchForm'
 import { Results } from '../components/Results/Results'
 
-import gql from 'graphql-tag';
-import { useLazyQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag'
+import { useLazyQuery } from '@apollo/react-hooks'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { Loader } from 'semantic-ui-react'
 
 const GET_SURPRISE_TRIPS = gql`
-query trips($originId: Int!, $travelDate: String!, $maxPrice: Int!, $withHalfFare: Boolean!, $categories: [String!]) {
-  getSurpriseTrips(originId: $originId, travelDate: $travelDate, maxPrice: $maxPrice, withHalfFare: $withHalfFare, categories: $categories) {
+  query trips(
+    $originId: Int!
+    $travelDate: String!
+    $maxPrice: Int!
+    $withHalfFare: Boolean!
+    $categories: [String!]
+  ) {
+    getSurpriseTrips(
+      originId: $originId
+      travelDate: $travelDate
+      maxPrice: $maxPrice
+      withHalfFare: $withHalfFare
+      categories: $categories
+    ) {
       price
       discount
       start
       end
       categories {
-          hiking
-          playing
-          sightseeing
-          eating
-          going_out
+        hiking
+        playing
+        sightseeing
+        eating
+        going_out
       }
+    }
   }
-}
 `
 
-interface Props { }
+interface Props {}
 
 export const Landing: React.FC<Props> = () => {
-
-  const [queryResults, { called, loading, data }] = useLazyQuery(GET_SURPRISE_TRIPS);
+  const [queryResults, { called, loading, data }] = useLazyQuery(
+    GET_SURPRISE_TRIPS,
+  )
 
   const searchTrips = (
     originId: number,
     travelDate: string,
     maxPrice: number,
     withHalfFare: boolean,
-    categories: string[]
+    categories: string[],
   ): void => {
     queryResults({
       variables: {
@@ -45,20 +57,15 @@ export const Landing: React.FC<Props> = () => {
         travelDate,
         maxPrice,
         withHalfFare,
-        categories
-      }
+        categories,
+      },
     })
   }
 
   return (
     <div>
       <SearchForm searchTrips={searchTrips} />
-
-      {called && loading &&
-        <div className="fa-3x" style={{ textAlign: 'center' }}>
-          <FontAwesomeIcon icon={faSpinner} className="fa-spin" />
-        </div>
-      }
+      {called && loading && <Loader active />}
       {called && data && <Results results={data.getSurpriseTrips} />}
     </div>
   )
