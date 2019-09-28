cb7f34df2f1ca6bb7b95a62c96dec4bb1725acb4
Roland Schlaefli <rolandschlaefli@gmail.com>
2019-09-28 21:20:15 +0200

    Replace flatMap with lodash flatten

--- backend/src/services/trips.ts
+++ backend/src/services/trips.ts
@@ -1,6 +1,8 @@
 import * as SygicService from './sygic'
 import * as SBBService from './sbb'
 import { minBy, maxBy } from 'lodash'
+import _flatten from 'lodash/flatten'
+import dayjs from 'dayjs'
 const util = require('util')
 
 async function getSurpriseTrips({
@@ -10,6 +12,8 @@ async function getSurpriseTrips({
   withHalfFare,
   categories,
 }) {
+  const formattedDate = dayjs(travelDate).format('YYYY-MM-DD')
+
   const matchingPlaces = await SygicService.getMatchingPlaces({
     categories: categories.join('|'),
   })
@@ -23,12 +27,12 @@ async function getSurpriseTrips({
 
   // query the top 10 places for trips
   const result = await Promise.all(
-    reachablePlaces.flatMap(async place => {
+    reachablePlaces.map(async place => {
       // get the best outgoing trip
       const bestOut = await SBBService.getBestPrices(
         originId,
         place.id,
-        travelDate,
+        formattedDate,
         ['06:00', '07:00', '08:00', '09:00'],
         maxPrice,
         withHalfFare,
@@ -42,7 +46,7 @@ async function getSurpriseTrips({
       const bestReturn = await SBBService.getBestPrices(
         place.id,
         originId,
-        travelDate,
+        formattedDate,
         ['18:00', '19:00', '20:00', '21:00'],
         maxPrice,
         withHalfFare,
@@ -51,8 +55,6 @@ async function getSurpriseTrips({
       if (bestReturn.length === 0 || !bestReturn.superSaver) {
         return []
       }
-
-      console.log(bestReturn)
 
       return {
         bestOut,
@@ -66,7 +68,7 @@ async function getSurpriseTrips({
     }),
   )
 
-  return result.flatMap(res => res)
+  return _flatten(result)
 }
 
 export { getSurpriseTrips }
